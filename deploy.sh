0bdfc3596fc0dab58f648ba8244a83a297457e86
Tak <env.tak@gmail.com>
2019-10-15 18:37:19 +0900

    chore: add deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,27 +1,25 @@
-#!/bin/bash
+#!/bin/sh
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+# If a command fails then the deploy stops
+set -e
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
 
 # Build the project.
-hugo -t learn
-
-# Copy assets
-yes | cp -rf assets public
+hugo -t learn # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
 cd public
+
 # Add changes to git.
-git add -A
+git add .
 
 # Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
 fi
 git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
-
-# Come Back
-cd ..
